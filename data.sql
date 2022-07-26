beb804ca8900212cedcb46e01107ec8e776a390a
franclobo <fjbl2788@gmail.com>
2022-07-26 08:44:46 -0500

    Correct date_of_birth

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 INSERT INTO public.animals (
 name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-'Agumon'::text, '02/03/2020'::date, '0'::integer, true::boolean, '10.23'::numeric)
+'Agumon'::text, '03/02/2020'::date, '0'::integer, true::boolean, '10.23'::numeric)
 returning id;
 INSERT INTO public.animals (
 name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
@@ -8,7 +8,7 @@ name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
 returning id;
 INSERT INTO public.animals (
 name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-'Picachu'::text, '01/07/2021'::date, '1'::integer, false::boolean, '15.04'::numeric)
+'Pikachu'::text, '07/01/2021'::date, '1'::integer, false::boolean, '15.04'::numeric)
 returning id;
 INSERT INTO public.animals (
 name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
