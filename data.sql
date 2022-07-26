6e8e505fc95558e7552418974f318eecdb328ebf
franclobo <fjbl2788@gmail.com>
2022-07-26 08:13:19 -0500

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.animals (
+name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Agumon'::text, '02/03/2020'::date, '0'::integer, true::boolean, '10.23'::numeric)
+returning id;
+INSERT INTO public.animals (
+name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Gabumon'::text, '15/11/2018'::date, '2'::integer, true::boolean, '8'::numeric)
+returning id;
+INSERT INTO public.animals (
+name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Devimon'::text, '12/05/2017'::date, '5'::integer, true::boolean, '10.23'::numeric)
+returning id;
+INSERT INTO public.animals (
+name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Agumon'::text, '03/02/2020'::date, '0'::integer, true::boolean, '10.23'::numeric)
+returning id;
