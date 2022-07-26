782f2996dafc45ceae4585058dadc481f6bbacb9
franclobo <fjbl2788@gmail.com>
2022-07-26 08:42:51 -0500

    Correct data.sql

--- data.sql
+++ data.sql
@@ -8,9 +8,9 @@ name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
 returning id;
 INSERT INTO public.animals (
 name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-'Devimon'::text, '12/05/2017'::date, '5'::integer, true::boolean, '10.23'::numeric)
+'Picachu'::text, '01/07/2021'::date, '1'::integer, false::boolean, '15.04'::numeric)
 returning id;
 INSERT INTO public.animals (
 name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
-'Agumon'::text, '03/02/2020'::date, '0'::integer, true::boolean, '10.23'::numeric)
+'Devimon'::text, '12/05/2017'::date, '5'::integer, true::boolean, '10.23'::numeric)
 returning id;
