f45b640d3390fd45eb251cb60d73d0bfb6c4cafa
franclobo <fjbl2788@gmail.com>
2022-07-27 16:26:06 -0500

    Modify schema.sql.

--- data.sql
+++ data.sql
@@ -14,3 +14,17 @@ INSERT INTO public.animals (
 name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
 'Devimon'::text, '12/05/2017'::date, '5'::integer, true::boolean, '10.23'::numeric)
 returning id;
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Charmander', '08/02/2020', '0', false, '-11')returning id;
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Plantmon', '15/11/2021', '2', true, '-5.7')returning id;
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Squirtle', '02/04/1993', '3', false, '-12.13')returning id;
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Angemon', '12/06/2005', '1', true, '-45')returning id;
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Boarmon', '07/06/2005', '7', true, '20.4')returning id;
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Blossom', '13/10/1998', '3', true, '17')returning id;
+INSERT INTO public.animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (
+'Ditto', '14/05/2022', '4', true, '22')returning id;
